e0a1181dd797f6821dd584790ca5ebec9da18946
hamidafghan <abd.hamidafghan@gmail.com>
2022-09-10 22:34:42 +0200

    Type conversion and input

---
+++ input.py
@@ -0,0 +1,9 @@
+name = input("What is your name: ")
+last_name = input("What is your last name: ")
+
+print(name, last_name)
+
+number1 = input("Enter Number 1: ")
+number2 = input("Enter Number 2: ")
+
+print(int(number1) + int(number2))
---
+++ type.py
@@ -0,0 +1,24 @@
+# Integer Types
+student_id = 31
+print("Id: ", student_id)
+print("Type: ", type(student_id))
+
+# String types
+student_name = "Jamal"
+print("Name: ", student_name)
+print("Type:", type(student_name))
+
+# Float type
+height = 180.5
+print("Height: ", height)
+print("Type: ", type(height))
+
+# Type conversation (integer)
+student_year = "4"
+x = int(student_year)
+print("Year: ", x)
+print("Type: ", type(x))
+
+# Conver to float
+y = float(student_year)
+print("Float type: ", type(y))
