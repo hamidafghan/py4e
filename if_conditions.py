0027dfbb9b49b9a2c311476cd9b1395651e1d82b
hamidafghan <abd.hamidafghan@gmail.com>
2022-09-13 22:12:27 +0200

    If condition

---
+++ if_conditions.py
@@ -0,0 +1,15 @@
+# Get the guest current age
+current_age = input("How old are you? ") # "15"
+
+# Convert the input to integer
+current_age = int(current_age) # 15
+
+# Check if the guest can enter to the pary
+if current_age != 21 :
+    print("Welcome")
+    print("Nice")
+else :
+    print("Sorry, you are NOT allowed to enter. Thanks")
+
+# Give a thank you to guest
+print("Hey, thanks you!")
