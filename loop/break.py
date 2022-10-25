c6d2d9aa71e5e84a6f2657f7e2898b0a07e09ab3
hamidafghan <abd.hamidaf@gmail.com>
2022-10-25 20:45:36 +0200

    The break statement

---
+++ loop/break.py
@@ -0,0 +1,15 @@
+numbers = [1,2,3,4,5,6,7,8,9,10]
+students = ["Jamal","asad", "Kamal", "Ali", "parwin"]
+
+# For
+# for number in numbers:
+
+#     if (number == 5):
+#         break
+
+#     print(number)
+
+for student in students:
+    if(student == "asad"):
+        print("Found asad")
+        break
