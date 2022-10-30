e5a3b0a6b03cbc240a65663da0dbd7ef471f3f11
hamidafghan <abd.hamidaf@gmail.com>
2022-10-30 20:12:10 +0100

    String Part 1

---
+++ strings/strings.py
@@ -0,0 +1,60 @@
+name = "Jamal"  # ['J', 'a', 'm', 'a', 'l']
+last_name = "Kamal"
+email = "jamal.gmail@icloud.com"
+bio = "I am a software engineer."
+
+# age = input("Enter age: ")
+
+# Print the type of name
+# print(type(name))
+
+# full_name = name + " " + last_name # Jamal Kamal
+# print(full_name)
+# print(type(int(age))) # 11
+
+# Looking inside the string
+# Jamal = [J,a,m,a,l]
+
+# first_letter = name[0]
+# end_letter = name[10]  # Err, index out of range
+# print(end_letter)
+
+# Length of string len() function
+# print(len(name)) # Jamal 5, lenght - 1 is index
+
+# print(len((1,2,4,67,7)))
+
+# length 5
+# index 4
+
+# loop over string
+# for letter in name:
+#     print(letter)
+
+# index = 0
+# while index < len(name):
+#     print(name[index], index)
+#     index = index + 1
+
+
+# a part of string
+# username = email[0:11]
+# domain = email[12:]
+# print(username)
+# print(domain)
+
+# Check if a letter is in string (IN)
+# print('gmail' in email)
+# if "@gmail.com" in email:
+#     print("The email is from google.")
+# else:
+#     print("The email is not form google.")
+
+# if name == "Jamal":
+#     print("Yes the name is correct")
+
+# if bio < "software":
+#     print("Yes")
+
+# Case conversation
+print(name.upper())
